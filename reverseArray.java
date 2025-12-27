3a17caf55c0f9553fd79ac7dcd4e4b6cf9f78683
OshankAgrawal <oshankagrawal@gmail.com>
2025-12-27 13:16:30 +0530

    Reverse Array

--- reverseArray.java
+++ reverseArray.java
@@ -1,24 +1,8 @@
 import java.util.Scanner;
 
 public class reverseArray{
-    public static void main(String[] args) {
-        Scanner obj=new Scanner(System.in);
-        String str=obj.nextLine();
-        String strarray[]=str.split(",");
-        int arr[]=new int[strarray.length];
-        for(int i=0;i<strarray.length;i++){
-            arr[i]=Integer.parseInt(strarray[i]);
-        }
-        SolutionReverseArray sc=new SolutionReverseArray();
-        sc.reverseArray(arr);
-        for(int num:arr){
-            System.out.print(num+" ");
-        }
-        obj.close();
-    }
-}
-class SolutionReverseArray {
-    public void reverseArray(int arr[]) {
+
+    public static void getreverseArray(int arr[]) {
         // code here
         int len=arr.length-1;
         int i=0;
@@ -29,5 +13,22 @@ class SolutionReverseArray {
             i++;
             len--;
         }
+    }
+
+    public static void main(String[] args) {
+        Scanner obj=new Scanner(System.in);
+        String str=obj.nextLine();
+        String strarray[]=str.split(",");
+        int arr[]=new int[strarray.length];
+        for(int i=0;i<strarray.length;i++){
+            arr[i]=Integer.parseInt(strarray[i]);
+        }
+
+        
+        getreverseArray(arr);
+        for(int num:arr){
+            System.out.print(num+" ");
+        }
+        obj.close();
     }
 }
